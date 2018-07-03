d11d62c3e5083702635621eca31f791b4ec67d9e
Applink-Group <36514240+Applink-Group@users.noreply.github.com>
2018-07-03 14:09:51 -0400

    update

--- FingerprintsModel/Transition.cs
+++ FingerprintsModel/Transition.cs
@@ -205,14 +205,14 @@ namespace FingerprintsModel
     /// <summary>
     /// Represents a file that has uploaded by a client via multipart/form-data. 
     /// </summary>
-    public class HttpPostedFileMultipart : HttpPostedFileBase
-    {
-        private readonly MemoryStream _fileContents;
+  //  public class HttpPostedFileMultipart : HttpPostedFileBase
+ //   {
+ //       private readonly MemoryStream _fileContents;
 
-        public override int ContentLength => (int)_fileContents.Length;
-        public override string ContentType { get; }
-        public override string FileName { get; }
-        public override Stream InputStream => _fileContents;
+ //       public override int ContentLength => (int)_fileContents.Length;
+  //      public override string ContentType { get; }
+  //      public override string FileName { get; }
+  //      public override Stream InputStream => _fileContents;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="HttpPostedFileMultipart"/> class. 
@@ -220,11 +220,11 @@ namespace FingerprintsModel
         /// <param name="fileName">The fully qualified name of the file on the client</param>
         /// <param name="contentType">The MIME content type of an uploaded file</param>
         /// <param name="fileContents">The contents of the uploaded file.</param>
-        public HttpPostedFileMultipart(string fileName, string contentType, byte[] fileContents)
-        {
-            FileName = fileName;
-            ContentType = contentType;
-            _fileContents = new MemoryStream(fileContents);
-        }
-    }
+     //   public HttpPostedFileMultipart(string fileName, string contentType, byte[] fileContents)
+      //  {
+      //      FileName = fileName;
+     // //      ContentType = contentType;
+     //       _fileContents = new MemoryStream(fileContents);
+      //  }
+   // }
 }
